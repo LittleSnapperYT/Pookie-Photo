435e1f72cc3572a4f9cee7bc823badf2969c3277
Jake <jakebattkicksass@gmail.com>
2022-04-18 21:02:58 -0600

    Ui Update

--- Pookie Photos/Form1.Designer.cs
+++ Pookie Photos/Form1.Designer.cs
@@ -28,12 +28,128 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.titlebar = new System.Windows.Forms.Panel();
+            this.Settingsbut = new System.Windows.Forms.Button();
+            this.minibut = new System.Windows.Forms.Button();
+            this.exitbut = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.titlebar.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // titlebar
+            // 
+            this.titlebar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(76)))), ((int)(((byte)(84)))), ((int)(((byte)(84)))));
+            this.titlebar.Controls.Add(this.Settingsbut);
+            this.titlebar.Controls.Add(this.minibut);
+            this.titlebar.Controls.Add(this.exitbut);
+            this.titlebar.Dock = System.Windows.Forms.DockStyle.Top;
+            this.titlebar.Location = new System.Drawing.Point(0, 0);
+            this.titlebar.Name = "titlebar";
+            this.titlebar.Size = new System.Drawing.Size(1050, 25);
+            this.titlebar.TabIndex = 0;
+            this.titlebar.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            // 
+            // Settingsbut
+            // 
+            this.Settingsbut.Dock = System.Windows.Forms.DockStyle.Right;
+            this.Settingsbut.FlatAppearance.BorderSize = 0;
+            this.Settingsbut.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(55)))), ((int)(((byte)(119)))), ((int)(((byte)(255)))));
+            this.Settingsbut.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Settingsbut.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Settingsbut.ForeColor = System.Drawing.Color.White;
+            this.Settingsbut.Location = new System.Drawing.Point(907, 0);
+            this.Settingsbut.Name = "Settingsbut";
+            this.Settingsbut.Size = new System.Drawing.Size(45, 25);
+            this.Settingsbut.TabIndex = 3;
+            this.Settingsbut.Text = "⚙";
+            this.Settingsbut.UseVisualStyleBackColor = true;
+            // 
+            // minibut
+            // 
+            this.minibut.BackColor = System.Drawing.Color.Transparent;
+            this.minibut.Dock = System.Windows.Forms.DockStyle.Right;
+            this.minibut.FlatAppearance.BorderSize = 0;
+            this.minibut.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(55)))), ((int)(((byte)(119)))), ((int)(((byte)(255)))));
+            this.minibut.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.minibut.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.minibut.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(246)))), ((int)(((byte)(248)))), ((int)(((byte)(255)))));
+            this.minibut.Location = new System.Drawing.Point(952, 0);
+            this.minibut.Name = "minibut";
+            this.minibut.Size = new System.Drawing.Size(49, 25);
+            this.minibut.TabIndex = 2;
+            this.minibut.Text = "-";
+            this.minibut.UseVisualStyleBackColor = false;
+            this.minibut.Click += new System.EventHandler(this.minibut_Click);
+            // 
+            // exitbut
+            // 
+            this.exitbut.BackColor = System.Drawing.Color.Transparent;
+            this.exitbut.Dock = System.Windows.Forms.DockStyle.Right;
+            this.exitbut.FlatAppearance.BorderSize = 0;
+            this.exitbut.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.exitbut.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.exitbut.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.exitbut.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(246)))), ((int)(((byte)(248)))), ((int)(((byte)(255)))));
+            this.exitbut.Location = new System.Drawing.Point(1001, 0);
+            this.exitbut.Name = "exitbut";
+            this.exitbut.Size = new System.Drawing.Size(49, 25);
+            this.exitbut.TabIndex = 1;
+            this.exitbut.Text = "X";
+            this.exitbut.UseVisualStyleBackColor = false;
+            this.exitbut.Click += new System.EventHandler(this.exitbut_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(438, 270);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Welcome, ";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 31);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1026, 540);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(164)))), ((int)(((byte)(172)))), ((int)(((byte)(150)))));
+            this.ClientSize = new System.Drawing.Size(1050, 583);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.titlebar);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Pookie Photo";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.titlebar.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Panel titlebar;
+        private Button minibut;
+        private Button exitbut;
+        private Label label1;
+        private Button Settingsbut;
+        private PictureBox pictureBox1;
     }
 }
--- Pookie Photos/Form1.cs
+++ Pookie Photos/Form1.cs
@@ -1,10 +1,80 @@
 namespace Pookie_Photos
 {
+
     public partial class Form1 : Form
     {
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+
+        private const int cGrip = 16;  
+        private const int cCaption = 32; 
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+
         public Form1()
         {
             InitializeComponent();
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.DoubleBuffered = true;
+            this.SetStyle(ControlStyles.ResizeRedraw, true);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Text = "Welcome to Pookie Photos, " + Environment.UserName;
+        }
+
+        private void exitbut_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void minibut_Click(object sender, EventArgs e)
+        {
+           this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            if (m.Msg == 0x84)
+            {  
+                Point pos = new Point(m.LParam.ToInt32());
+                pos = this.PointToClient(pos);
+                if (pos.Y < cCaption)
+                {
+                    m.Result = (IntPtr)2;
+                    return;
+                }
+                if (pos.X >= this.ClientSize.Width - cGrip && pos.Y >= this.ClientSize.Height - cGrip)
+                {
+                    m.Result = (IntPtr)17;
+                    return;
+                }
+            }
+            base.WndProc(ref m);
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            Rectangle rc = new Rectangle(this.ClientSize.Width - cGrip, this.ClientSize.Height - cGrip, cGrip, cGrip);
+            ControlPaint.DrawSizeGrip(e.Graphics, this.BackColor, rc);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
